a1f644faa00f3bf74828c0cdd1275d1fea36f68e
Winson <winsonheng@hotmail.com>
2018-10-14 17:39:24 +0800

    Created simple login page

---
+++ src/components/Login.vue
@@ -0,0 +1,58 @@
+<template>
+    <el-form :model="loginDetails" :rules="rules" ref="loginDetails">
+        <el-form-item label="Username" prop="username">
+            <el-input v-model="loginDetails.username"></el-input>
+        </el-form-item>
+        <el-form-item label="Password" prop="password">
+            <el-input v-model="loginDetails.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" @click="submitForm('loginDetails')">Create</el-button>
+        </el-form-item>
+    </el-form>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      loginDetails: {
+        username: "",
+        password: "",
+      },
+      rules: {
+        username: [
+          {
+            required: true,
+            message: "Please input Activity name",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: "Please input password",
+            trigger: "blur",
+          },
+        ],
+      },
+    }
+  },
+  methods: {
+    submitForm(loginDetails) {
+      console.log(
+        this.$refs[loginDetails].validate((valid) => {
+          if (valid) {
+            alert("valid")
+          }
+          alert("invaid")
+        })
+      )
+    },
+  },
+}
+</script>
+
+<style>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue"
 import Router from "vue-router"
 import Attendance from "@/views/AttendancePage"
+import Login from "@/components/Login"
 
 Vue.use(Router)
 
@@ -9,7 +10,7 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Attendance,
+      component: Login,
     },
     {
       path: "/about",
