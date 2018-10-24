9651506c6aa8b945af8ab771b3b36e4db177da86
Winson <winsonheng@hotmail.com>
2018-10-24 21:20:26 +0800

    Wrote stuff for vuex store. Added firebase api

--- src/common/data.js
+++ src/common/data.js
@@ -7,3 +7,12 @@ export const DAY = {
   SATURDAY: "SATURDAY",
   SUNDAY: "SUNDAY",
 }
+
+export const firebaseConfig = {
+  apiKey: process.env.firebaseConfig,
+  authDomain: "taekwandobackend-d1edf.firebaseapp.com",
+  databaseURL: "https://taekwandobackend-d1edf.firebaseio.com",
+  projectId: "taekwandobackend-d1edf",
+  storageBucket: "taekwandobackend-d1edf.appspot.com",
+  messagingSenderId: "1011970784530",
+}
--- src/store.js
+++ src/store.js
@@ -1,10 +1,32 @@
 import Vue from "vue"
 import Vuex from "vuex"
+import firebase from "firebase"
+import { firebaseConfig } from "@/common/data"
+import { Promise } from "core-js"
+
+const firebaseDB = firebase.initializeApp(firebaseConfig)
+const usersRef = firebaseDB.database().ref("Users")
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    students: [],
+  },
+  mutations: {
+    saveStudentData(state, studentData) {
+      state.students = studentData
+    },
+  },
+  actions: {
+    loadStudentData({ commit, state }) {
+        try {
+          const usersArray = usersRef.on("value", (snapshot) => snapshot.val())
+          commit("saveStudentData", usersArray)
+        } catch {
+          console.log("users retrieval failed")
+        }
+      })
+    },
+  },
 })
